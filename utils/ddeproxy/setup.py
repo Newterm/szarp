0bd03a9f0307f013ee7fadb5ae2fa010b85c8ab2
pawel <pawel@newterm.pl>
2010-08-18 11:28:12 +0200

    Adding DLL's to ddeproxy py2exe instalation.
    	py2exe setup file was modified to include all neccessary DLL's.

--- utils/ddeproxy/setup.py
+++ utils/ddeproxy/setup.py
@@ -5,13 +5,23 @@
 # If everything works well, you should find a subdirectory named 'dist'
 # containing output files.
 
+# modify it to suite your environment
+mfcdir = r'C:\Python27\Lib\site-packages\pythonwin\\'
+
 from distutils.core import setup
 import py2exe
+
+mfcfiles = map(lambda x: mfcdir + x, ["mfc90.dll", "mfc90u.dll", "mfcm90.dll",
+                                      "mfcm90u.dll",
+                                      "Microsoft.VC90.MFC.manifest"])
+
+data_files = [ ("Microsoft.VC90.MFC", mfcfiles), ]
 
 setup(
     # The first three parameters are not required, if at least a
     # 'version' is given, then a versioninfo resource is built from
     # them and added to the executables.
+    data_files = data_files,
     version = "0.1",
     description = "SZARP DDE Proxy",
     name = "ddeproxy",
