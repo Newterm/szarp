c1b1a8be9994b5b3eab4ced55feb7fa70cedb4dc
Jakub Kotur <qba@newterm.pl>
2012-04-03 10:57:20 +0200

    Python 3 compatibility

--- pyipk/libipk/params.py
+++ pyipk/libipk/params.py
@@ -8,7 +8,10 @@ try :
 except ImportError :
 	import urllib.parse as up
 
-from StringIO import StringIO
+try :
+	from StringIO import StringIO
+except ImportError :
+	from io import StringIO
 
 import lxml
 
