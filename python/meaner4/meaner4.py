d6100b9eab6b30b47e12d2bfe1762386c38d2642
Darek Marcinkiewicz <reksio@newterm.pl>
2012-06-19 16:55:51 +0200

    menaer4
    	* python/meaner4: beginnings/prototype of yet another incarnation
    	of meaner, meaner4 this time

---
+++ python/meaner4/meaner4.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+"""
+  SZARP: SCADA software 
+  Darek Marcinkiewicz <reksio@newterm.pl>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+
+"""
+
+import sys
+import meaner
+
+sys.path.append("/opt/szarp/lib/python")
+
+from libpar import LibparReader
+
+if __name__ == "__main__":
+	lpr = LibparReader()
+
+	path = lpr.get("global", "szbase")
+	ipk = lpr.get("global", "IPK")
+
+	uri = lpr.get("global", "paramd_uri")
+
+	meaner = meaner.Meaner(uri)
+	meaner.configure(ipk)
+
+	menaer.run()
+
