3a14002b0abf9f92ed357ebccd594c9905fb8a9f
Darek Marcinkiewicz <reksio@newterm.pl>
2012-06-21 09:30:26 +0200

    changes, back to initial idea of db format
    switch to initial idea of db format

--- python/meaner4/saveparam.py
+++ python/meaner4/saveparam.py
@@ -19,118 +19,88 @@
 """
 
 import os
+import fcntl
 
 import config
 import param
 import parampath
 import math
 
-class SaveFile:
-	def __init__(self, path):
-		self.path = path
-		self.tmp_path = os.path.join(os.path.dirname(path), "meaner3.tmp")
-		self.file = None
-
-		if os.path.exists(path):
-			st = os.stat(path)
-			self.size = st.st_size
-			self.time = math.floor(st.st_mtime)
-			self.nanotime = (st.st_mtime - self.time) * (10 ** 9)
-			self.exists = True
-		else:
-			self.size = 0
-			self.time = None
-			self.nanotime = None
-			self.exists = False
-
-	def write(self, value, time, nanotime):
-		self.time = time
-		self.nanotime = nanotime
-
-		if self.exists:
-			os.rename(self.path, self.tmp_path)
-
-		with open(self.tmp_path, "a+") as f:
-			f.write(value)
-
-		os.utime(self.tmp_path, (time, time))
-		os.rename(self.tmp_path, self.path)
-
-		self.exists = True
-		self.size += len(value)
-
-	def update_mod_time(self, time, nanotime):
-		self.time = time
-		self.nanotime = nanotime
-
-		if nanotime is not None:
-			time = time + float(nanotime) / (10 ** 9)
-		os.utime(self.path, (time, time))
-		
-	def close(self):
-		if self.file is not None:
-			self.file.close()
-
-
 class SaveParam:
 	def __init__(self, node, szbase_dir):
 		self.param = param.Param(node)
 		self.param_path = parampath.ParamPath(self.param, szbase_dir)
 		self.file = None
-		self.last_value = None
+		self.current_value = None
+		self.first_write = True
 
-	def open_latest_file(self, time, nanotime):
-		path = self.param_path.find_latest_path()
-		if path is None:
-			try:
-				os.makedirs(self.param_path.param_dir())
-			except IOError:
-				pass
+	def update_last_time(self, time, nanotime):
+		self.file.seek(-self.param.time_prec, os.SEEK_END)
+		try:
+			fcntl.lockf(self.file, fcntl.LOCK_EX)
+			self.file.write(self.param.time_to_binary(time, nanotime))
+			self.file.flush()
+		finally:
+			fcntl.lockf(self.file, fcntl.LOCK_UN)
+
+	def new_value(self, value, time, nanotime):
+		if self.file_size + self.param.time_prec + self.param.value_lenght >= config.DATA_FILE_SIZE:
+			self.file.close()
 
 			path = self.param_path.create_file_path(time, nanotime)
+			self.file = open(path, "w+b")
+			self.file_size = 0
 
-		self.file = SaveFile(path)
+		try:
+			fcntl.lockf(self.file, fcntl.LOCK_EX)
+			self.file.write(self.param.value_to_binary(value))
+			self.file.write(self.param.time_to_binary(time, nanotime))
+			self.file.flush()
+	
+			self.file_size += self.param.time_prec + self.param.value_lenght
+		finally:
+			fcntl.lockf(self.file, fcntl.LOCK_UN)
 
-	def read_last_value(self):
-		if not self.file.exists or self.file.size == 0:
-			return
+		self.current_value = value
 
-		with open(self.file.path, 'r') as f:
-			f.seek(-self.param.value_lenght, os.SEEK_END)
-			self.last_value = self.param.value_from_binary(f.read(self.param.value_lenght))
+	def prepare_for_writing(self, time, nanotime):
+		path = self.param_path.find_latest_path()	
 
-	def add_new_value(self, new_val, msg):
-		new_val_blob = self.param.value_to_binary(new_val)
-		if self.last_value is not None:
-			old_time_blob = self.param.time_to_binary(self.file.time, self.file.nanotime)
+		if path is not None:
+			self.file_size = os.path.getsize(path)
+			self.file = open(path, "r+b")
 
-			if self.file.size + 2 * self.param.time_prec + self.param.value_lenght > config.DATA_FILE_SIZE:
-				self.file.write(old_time_blob, self.file.time, self.file.nanotime)
-				self.file.close()
+			if self.file_size >= self.param.time_prec + self.param.value_lenght:
+				self.file.seek(-(self.param.time_prec + self.param.value_lenght), os.SEEK_END)
+				value_blob = self.file.read(self.param.value_lenght)
+				self.file.seek(0, os.SEEK_END)
 
-				self.file = SaveFile(self.param_path.create_file_path(msg.time, msg.nanotime))
-				self.file.write(new_val_blob, msg.time, msg.nanotime)
+				self.current_value = self.param.value_from_binary(value_blob)
 			else:
-				self.file.write(old_time_blob + new_val_blob, msg.time, msg.nanotime)
+				self.current_value = None
 		else:
-				self.file.write(new_val_blob, msg.time, msg.nanotime)
-		self.last_value = new_val
+			self.file_size = 0
+
+			param_dir = self.param_path.param_dir()
+			if not os.path.exists(param_dir):
+				os.makedirs(param_dir)
+
+			path = self.param_path.create_file_path(time, nanotime)
+			self.file = open(path, "w+b")
+			self.current_value = None
+
 
 	def process_msg(self, msg):
 		if not self.param.written_to_base:
 			return
 
-		if self.file is None:
-			self.open_latest_file(msg.time, msg.nanotime)
+		if self.first_write:
+			self.prepare_for_writing(msg.time, msg.nanotime)
+			self.first_write = False
 
-		if self.last_value is None:
-			self.read_last_value()
-
-		new_val = self.param.value_from_msg(msg)
-		if  new_val == self.last_value:
-			self.file.update_mod_time(msg.time, msg.nanotime)
+		value = self.param.value_from_msg(msg)
+		if  value == self.current_value:
+			self.update_last_time(msg.time, msg.nanotime)
 		else:
-			self.add_new_value(new_val, msg)
-
-		
+			self.new_value(value, msg.time, msg.nanotime)
 
