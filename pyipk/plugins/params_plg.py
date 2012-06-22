eb06f0044a85411cac88ae7d9606c3679eae95b6
Jakub Kotur <qba@newterm.pl>
2012-06-22 09:59:41 +0200

    pyipk: LetterChecker plugin
    + plugin searches in param@name attrib for sections that has no
      difference while converting to plain ascii. That is helpful while
      there is a misspell in local character: "Kocioł 1" -> "Kociol 1"
    + if there is parameter specified plugin corrects all misspelled
      sections. It will change all sections that after converting to
      ascii are equal to one provided. If 'correct' is set to "Kocioł" it
      will change "Kociol" and "Kociół" to correct version.

--- pyipk/plugins/params_plg.py
+++ pyipk/plugins/params_plg.py
@@ -5,6 +5,8 @@
 from libipk.plugin import Plugin
 from lxml import etree
 
+import unicodedata
+
 import re
 
 class RenameParamName( Plugin ) :
@@ -85,8 +87,10 @@ class FindSubStrings( Plugin ) :
 			i += j
 		return out
 
-class CheckParamsForISL( FindSubStrings ) :
-	'''Finds params with names that are substrings of other params names. Thats sometimes an issue in ISL diagrams.
+class FindLswMsw( FindSubStrings ) :
+	'''Finds params with names that are substrings of other params names.
+
+	This is also useful while searching for lsw/msw parameters.
 	'''
 
 	@staticmethod
@@ -98,4 +102,83 @@ class CheckParamsForISL( FindSubStrings ) :
 	@staticmethod
 	def get_args() :
 		return []
+
+class LetterChecker( Plugin ) :
+	'''Finds params which sections differs but doesn't differ after converting from unicode to ascii
+
+	  * correct to - if not specified only shows nodes which differs, if set will change all sections which doesn't differ after converting to ascii to specified value
+	'''
+	def __init__( self , **args ) :
+		Plugin.__init__( self , **args )
+		self.lnodes = ( {} , {} , {} )
+		self.cnodes = []
+
+	@staticmethod
+	def section() : return 'Params'
+
+	@staticmethod
+	def get_args() :
+		return [ 'correct to' ]
+
+	def set_args( self , **args ) :
+		self.correct = args['correct to']
+		self.attrib = 'name'
+		self.xpath = ".//ns:%s[@%s]" % ( 'param' , 'name' )
+
+	def normalize_string( self , string ) :
+		return unicodedata.normalize('NFKD', unicode(string)).replace(u'ł', 'l').replace(u'Ł', 'L').encode('ascii','ignore')
+
+	def get_sections( self , name ) :
+		return name.split(':',3)
+
+	def check( self , a , b ) :
+		return self.normalize_string(a) == self.normalize_string(b)
+
+	def process( self , root ) :
+		if self.correct == '' :
+			self.process_list( root )
+		else :
+			self.process_correct( root )
+
+	def result( self ) :
+		if self.correct == '' :
+			return self.result_list() 
+		else :
+			return self.result_correct()
+		
+	def process_list( self , root ) :
+		for node in root.xpath( self.xpath , namespaces = { 'ns' : root.nsmap[None] } ) :
+			secs = self.get_sections( node.attrib[self.attrib] )
+			for i in range(3) :
+				value = self.normalize_string( secs[i] )
+				if value not in self.lnodes[i] :
+					self.lnodes[i][ value ]  = []
+
+				append = True
+				for n in self.lnodes[i][value] :
+					if self.get_sections( n.attrib[self.attrib] )[i] == secs[i] :
+						append = False
+						break
+				if append :
+					self.lnodes[i][ value ].append( node )
+
+	def result_list( self ) :
+		return reduce( lambda x , y : reduce( lambda a,b : a+b if len(b)>1 else a , y.values() , [] ) + x , self.lnodes , [] )
+
+	def process_correct( self , root ) :
+		correct_normal = self.normalize_string( self.correct )
+		for node in root.xpath( self.xpath , namespaces = { 'ns' : root.nsmap[None] } ) :
+			secs = self.get_sections( node.get(self.attrib) )
+			replace = False
+			for i in range(3) :
+				value = self.normalize_string( secs[i] )
+				if value == correct_normal and secs[i] != self.correct :
+					secs[i] = self.correct
+					replace = True
+			if replace :
+				node.set( self.attrib , ':'.join(secs) )
+				self.cnodes.append( node )
+	
+	def result_correct( self ) :
+		return self.cnodes
 
