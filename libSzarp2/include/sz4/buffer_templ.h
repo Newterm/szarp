61dd17a027872fd8209e5802637067c8ee72089f
Darek Marcinkiewicz <reksio@newterm.pl>
2012-10-19 13:55:16 +0000

    more files

---
+++ libSzarp2/include/sz4/buffer_templ.h
@@ -0,0 +1,130 @@
+#ifndef __SZ4_BUFFER_TEMPL_H__
+#define __SZ4_BUFFER_TEMPL_H__
+/* 
+  SZARP: SCADA software 
+  
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+*/
+
+namespace sz4 {
+
+template<template<typename DT, typename TT, class BT> class param_entry_type, class base_type, class data_type, class time_type> generic_param_entry* param_entry_build_t_3(base_type* base, TParam* param, const boost::filesystem::wpath &buffer_directory) {
+	return new param_entry_in_buffer<param_entry_type, data_type, time_type, base_type>(base, param, buffer_directory);
+}
+
+template<template <typename DT, typename TT, class BT> class param_entry_type, class base_type, class data_type> generic_param_entry* param_entry_build_t_2(base_type* base, TParam* param, const boost::filesystem::wpath &buffer_directory) {
+	switch (param->GetTimeType()) {
+		case TParam::SECOND:
+			return param_entry_build_t_3<param_entry_type, base_type, data_type, second_time_t>(base, param, buffer_directory);
+		case TParam::NANOSECOND:
+			return param_entry_build_t_3<param_entry_type, base_type, data_type, nanosecond_time_t>(base, param, buffer_directory);
+	}
+	return NULL;
+}
+
+template<template <typename DT, typename TT, class BT> class param_entry_type, class base_type> generic_param_entry* param_entry_build_t_1(base_type* base, TParam* param, const boost::filesystem::wpath &buffer_directory) {
+	switch (param->GetDataType()) {
+		case TParam::SHORT:
+			return param_entry_build_t_2<param_entry_type, base_type, short>(base, param, buffer_directory);
+		case TParam::INT:
+			return param_entry_build_t_2<param_entry_type, base_type, int>(base, param, buffer_directory);
+		case TParam::FLOAT:
+			return param_entry_build_t_2<param_entry_type, base_type, float>(base, param, buffer_directory);
+		case TParam::DOUBLE:
+			return param_entry_build_t_2<param_entry_type, base_type, double>(base, param, buffer_directory);
+	}
+	return NULL;
+}
+
+template<class base_type> generic_param_entry* param_entry_build(base_type *base, TParam* param, const boost::filesystem::wpath &buffer_directory) {
+	switch (param->GetSz4Type()) {
+		case TParam::SZ4_REAL:
+			return param_entry_build_t_1<real_param_entry_in_buffer, base_type>(base, param, buffer_directory);
+		case TParam::SZ4_LUA_OPTIMIZED: {
+			generic_param_entry* entry = param_entry_build_t_1<lua_optimized_param_entry_in_buffer, base_type>(base, param, buffer_directory);
+			LuaExec::Param* exec_param = param->GetLuaExecParam();
+			for (std::vector<LuaExec::ParamRef>::iterator i = exec_param->m_par_refs.begin();
+					i != exec_param->m_par_refs.end();
+					i++) {
+				generic_param_entry* reffered_entry = base->get_param_entry(i->m_param);
+				reffered_entry->add_reffering_param(entry);
+			}
+			return entry;
+		}
+		default:
+		case TParam::SZ4_NONE:
+			assert(false);
+	}
+}
+
+
+template<class ipk_container_type> void buffer_templ<ipk_container_type>::remove_param(TParam* param) {
+	if (m_param_ents.size() <= param->GetParamId())
+		return;
+
+	generic_param_entry* entry = m_param_ents[param->GetParamId()];
+	if (!entry)
+		return;
+
+	entry->deregister_from_monitor(m_param_monitor);
+	delete entry;
+
+	m_param_ents[param->GetParamId()] = NULL;
+}
+
+template<class ipk_container_type> generic_param_entry* buffer_templ<ipk_container_type>::create_param_entry(TParam* param) {
+	prepare_param(param);
+
+	return param_entry_build(m_base, param, m_buffer_directory);
+}
+
+template<class ipk_container_type> void buffer_templ<ipk_container_type>::prepare_param(TParam* param) {
+	if (param->GetSz4Type() != TParam::SZ4_NONE)
+		return;
+
+	if (param->GetType() == TParam::P_REAL) {
+		param->SetSz4Type(TParam::SZ4_REAL);
+		return;
+	}
+
+	if (param->IsDefinable())
+		param->PrepareDefinable();
+
+	if (param->GetType() == TParam::P_COMBINED) {
+		param->SetSz4Type(TParam::SZ4_COMBINED);
+		return;
+	}
+
+	if (param->GetType() == TParam::P_DEFINABLE) {
+		param->SetSz4Type(TParam::SZ4_DEFINABLE);
+		return;
+	}
+
+	if (param->GetType() == TParam::P_LUA) {
+		LuaExec::Param* exec_param = new LuaExec::Param();
+		param->SetLuaExecParam(exec_param);
+
+		if (LuaExec::optimize_lua_param<ipk_container_type>(param, m_ipk_container)) 
+			param->SetSz4Type(TParam::SZ4_LUA_OPTIMIZED);
+		else
+			param->SetSz4Type(TParam::SZ4_LUA);
+		return;
+	}
+}
+
+}
+
+#endif
---
+++ unit_tests/sz4_lua_param_optimized.cpp
@@ -0,0 +1,148 @@
+#include "config.h"
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <vector>
+#include <limits>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cppunit/extensions/HelperMacros.h>
+#include <boost/filesystem.hpp>
+
+#include "conversion.h"
+#include "szarp_config.h"
+#include "liblog.h"
+#include "szarp_base_common/defines.h"
+#include "szarp_base_common/lua_param_optimizer.h"
+#include "sz4/defs.h"
+#include "sz4/time.h"
+#include "sz4/path.h"
+#include "sz4/block.h"
+#include "sz4/buffer.h"
+#include "sz4/definable_param_cache.h"
+#include "szarp_base_common/lua_param_optimizer_templ.h"
+#include "sz4/real_param_entry.h"
+#include "sz4/lua_optimized_param_entry.h"
+#include "sz4/buffer_templ.h"
+#include "sz4/base.h"
+
+#include "test_serach_condition.h"
+
+class Sz4LuaParamOptimized : public CPPUNIT_NS::TestFixture
+{
+	void test1();
+	void test2();
+
+	CPPUNIT_TEST_SUITE( Sz4LuaParamOptimized );
+	CPPUNIT_TEST( test1 );
+	CPPUNIT_TEST( test2 );
+	CPPUNIT_TEST_SUITE_END();
+public:
+	void setUp();
+};
+
+namespace {
+
+class IPKContainerMock1 {
+	TParam param;
+	TSzarpConfig config;
+public:
+	IPKContainerMock1() : param(NULL, NULL, std::wstring(), TParam::LUA_VA, TParam::P_LUA) {
+		param.SetConfigId(0);
+		param.SetParamId(0);
+		param.SetDataType(TParam::DOUBLE);
+		param.SetName(L"A:B:C1");
+		param.SetLuaScript((const unsigned char*) 
+"if (t % 100) < 50 then "
+"	v = 0		"
+"else			"
+"	v = 1		"
+"end			"
+);
+		param.SetParentSzarpConfig(&config);
+
+		config.SetName(L"TEST", L"TEST");
+	}
+	TSzarpConfig* GetConfig(const std::wstring&) { return &config; }
+	TParam* GetParam(const std::wstring&) { return &param; }
+};
+
+}
+
+
+CPPUNIT_TEST_SUITE_REGISTRATION( Sz4LuaParamOptimized );
+
+void Sz4LuaParamOptimized::setUp() {
+}
+
+void Sz4LuaParamOptimized::test1() {
+	IPKContainerMock1 mock;
+	sz4::base_templ<IPKContainerMock1> base(L"", &mock);
+	sz4::buffer_templ<IPKContainerMock1>* buff = base.buffer_for_param(mock.GetParam(L""));
+
+	sz4::weighted_sum<double, sz4::second_time_t> sum;
+	buff->get_weighted_sum(mock.GetParam(L""), 100u, 200u, PT_SEC10, sum);
+	CPPUNIT_ASSERT_EQUAL(50., sum.sum());
+	CPPUNIT_ASSERT_EQUAL(sz4::time_difference<sz4::second_time_t>::type(100), sum.weight());
+	CPPUNIT_ASSERT_EQUAL(true, sum.fixed());
+}
+
+namespace unit_test {
+
+class IPKContainerMock2 {
+	TParam param;
+	TParam param2;
+	TSzarpConfig config;
+public:
+	IPKContainerMock2() : param(NULL, NULL, std::wstring(), TParam::NONE, TParam::P_REAL),
+				param2(NULL, NULL, std::wstring(), TParam::LUA_VA, TParam::P_LUA)
+	 {
+		param.SetConfigId(0);
+		param.SetParamId(0);
+		param.SetDataType(TParam::INT);
+		param.SetName(L"A:B:C");
+		param.SetParentSzarpConfig(&config);
+
+		param2.SetConfigId(0);
+		param2.SetParamId(1);
+		param2.SetDataType(TParam::DOUBLE);
+		param2.SetLuaScript((const unsigned char*) 
+"v = p(\"BASE:A:B:C\", t, pt)"
+);
+		param2.SetParentSzarpConfig(&config);
+
+		config.SetName(L"TEST", L"TEST");
+	}
+
+	TSzarpConfig* GetConfig(const std::wstring&) { return (TSzarpConfig*) 1; }
+
+	TParam* GetParam(const std::wstring& name) {
+		if (name == L"BASE:A:B:C")
+			return &param;
+
+		if (name == L"BASE:A:B:D")
+			return &param2;
+		
+		assert(false);
+		return NULL;
+	}
+};
+
+}
+
+
+void Sz4LuaParamOptimized::test2() {
+	unit_test::IPKContainerMock2 mock;
+	sz4::base_templ<unit_test::IPKContainerMock2> base(L"", &mock);
+
+	sz4::buffer_templ<unit_test::IPKContainerMock2>* buff = base.buffer_for_param(mock.GetParam(L"BASE:A:B:D"));
+
+	sz4::weighted_sum<double, sz4::second_time_t> sum;
+	buff->get_weighted_sum(mock.GetParam(L"BASE:A:B:D"), 100u, 200u, PT_SEC10, sum);
+	CPPUNIT_ASSERT_EQUAL(sz4::time_difference<sz4::second_time_t>::type(0), sum.weight());
+	CPPUNIT_ASSERT_EQUAL(sz4::time_difference<sz4::second_time_t>::type(100), sum.no_data_weight());
+	CPPUNIT_ASSERT_EQUAL(false, sum.fixed());
+
+}
+
