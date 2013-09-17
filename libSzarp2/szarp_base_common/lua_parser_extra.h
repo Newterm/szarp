d88b61e27d96722914678c5cc44da92948c6caeb
Darek Marcinkiewicz <reksio@newterm.pl>
2013-09-17 22:41:11 +0200

    *sz4: latest splitting of the parser introduced a bug
    this is the fix

--- libSzarp2/szarp_base_common/lua_parser_extra.h
+++ libSzarp2/szarp_base_common/lua_parser_extra.h
@@ -20,19 +20,6 @@ qi::rule<std::wstring::const_iterator, std::wstring(), lua_skip_parser> string_r
 	qi::rule<std::wstring::const_iterator, wchar_t()>& escaped_character
 	);
 
-void operators_precedence(
-	qi::rule<std::wstring::const_iterator, expression(), lua_skip_parser>& expression_,
-	qi::rule<std::wstring::const_iterator, or_exp(), lua_skip_parser>& or_,
-	qi::rule<std::wstring::const_iterator, and_exp(), lua_skip_parser>& and_,
-	qi::rule<std::wstring::const_iterator, cmp_exp(), lua_skip_parser>& cmp_,
-	qi::rule<std::wstring::const_iterator, concat_exp(), lua_skip_parser>& concat_,
-	qi::rule<std::wstring::const_iterator, add_exp(), lua_skip_parser>& add_,
-	qi::rule<std::wstring::const_iterator, mul_exp(), lua_skip_parser>& mul_,
-	qi::rule<std::wstring::const_iterator, unop_exp(), lua_skip_parser>& unop_,
-	qi::rule<std::wstring::const_iterator, pow_exp(), lua_skip_parser>& pow_,
-	qi::rule<std::wstring::const_iterator, term(), lua_skip_parser>& term_
-);
-
 struct mulop_symbols : qi::symbols<wchar_t, lua_grammar::mul_op> {
 	mulop_symbols ();
 };
@@ -52,6 +39,24 @@ struct unop_symbols : qi::symbols<wchar_t, lua_grammar::un_op> {
 struct escaped_symbol : qi::symbols<wchar_t, wchar_t> {
 	escaped_symbol();
 };
+
+void operators_precedence(
+	qi::rule<std::wstring::const_iterator, expression(), lua_skip_parser>& expression_,
+	qi::rule<std::wstring::const_iterator, or_exp(), lua_skip_parser>& or_,
+	qi::rule<std::wstring::const_iterator, and_exp(), lua_skip_parser>& and_,
+	qi::rule<std::wstring::const_iterator, cmp_exp(), lua_skip_parser>& cmp_,
+	qi::rule<std::wstring::const_iterator, concat_exp(), lua_skip_parser>& concat_,
+	qi::rule<std::wstring::const_iterator, add_exp(), lua_skip_parser>& add_,
+	qi::rule<std::wstring::const_iterator, mul_exp(), lua_skip_parser>& mul_,
+	qi::rule<std::wstring::const_iterator, unop_exp(), lua_skip_parser>& unop_,
+	qi::rule<std::wstring::const_iterator, pow_exp(), lua_skip_parser>& pow_,
+	qi::rule<std::wstring::const_iterator, term(), lua_skip_parser>& term_,
+	addop_symbols** addop_symbols_,
+	cmpop_symbols** cmpop_symbols_,
+	mulop_symbols** mulop_symbols_,
+	unop_symbols** unop_symbols_
+);
+
 
 }
 
