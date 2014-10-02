66a0d581e27f60a7f8580d2c830991715f116c1f
Tomasz Pieczerak <tph@newterm.pl>
2014-10-02 17:32:44 +0200

    filler2: Qt4 version of previous work
    * filler2 will be the first tool for SZARP written in Qt4 :)

---
+++ qt4/filler2/filler2.pro
@@ -0,0 +1,3 @@
+SOURCES = filler2.py
+
+TRANSLATIONS = filler2_pl.ts
---
+++ qt4/filler2/pl/filler2_pl.ts
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pl_PL">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="filler2.py" line="104"/>
+        <source>Filler 2</source>
+        <translation>Filler 2</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="105"/>
+        <source>&amp;File</source>
+        <translation>&amp;Plik</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="106"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Pomoc</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="107"/>
+        <source>toolBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="108"/>
+        <source>&amp;Save data</source>
+        <translation>&amp;Zapisz dane</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="109"/>
+        <source>Save data to params.xml</source>
+        <translation>Zapisz dane do pliku params.xml</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="110"/>
+        <source>Ctrl+S</source>
+        <translation>Ctrl+S</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="111"/>
+        <source>C&amp;lear</source>
+        <translation>Wy&amp;czyść</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="112"/>
+        <source>Clear pending actions</source>
+        <translation>Wyczyść zaplanowane akcje</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="113"/>
+        <source>Ctrl+C</source>
+        <translation>Ctrl+C</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="114"/>
+        <source>E&amp;xit</source>
+        <translation>Za&amp;kończ</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="115"/>
+        <source>Exit Filler 2</source>
+        <translation>Zakończ program Filler 2</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="116"/>
+        <source>Ctrl+Q</source>
+        <translation>Ctrl+Q</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="117"/>
+        <source>User&apos;s &amp;manual</source>
+        <translation>Pod&amp;ręcznik użytkownika</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="118"/>
+        <source>Show user&apos;s manual</source>
+        <translation>Pokaż podręcznik użytkownika</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="120"/>
+        <source>Con&amp;text help</source>
+        <translation>Pomoc &amp;kontekstowa</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="121"/>
+        <source>Activate context help</source>
+        <translation>Uruchom pomoc kontekstową</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="123"/>
+        <source>&amp;About</source>
+        <translation>&amp;O programie</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="124"/>
+        <source>Information about Filler 2</source>
+        <translation>Informacje o programie Filler 2</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="125"/>
+        <source>About &amp;Qt</source>
+        <translation>O &amp;Qt</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="126"/>
+        <source>Information about Qt4</source>
+        <translation>Informacje o Qt4</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="119"/>
+        <source>F1</source>
+        <translation>F1</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="122"/>
+        <source>Shift+F1</source>
+        <translation>Shift+F1</translation>
+    </message>
+</context>
+</TS>
