84f7b7cb9f2a9a4afb30e04fa0f8065df96d3708
Tomasz Pieczerak <tph@newterm.pl>
2014-10-06 16:06:06 +0200

    filler2: finished user interface look
    * added needed UI components.
    * updated Polish translation.

--- qt4/filler2/pl/filler2_pl.ts
+++ qt4/filler2/pl/filler2_pl.ts
@@ -4,119 +4,204 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="filler2.py" line="104"/>
+        <location filename="filler2.py" line="164"/>
         <source>Filler 2</source>
         <translation>Filler 2</translation>
     </message>
     <message>
-        <location filename="filler2.py" line="105"/>
+        <location filename="filler2.py" line="182"/>
         <source>&amp;File</source>
         <translation>&amp;Plik</translation>
     </message>
     <message>
-        <location filename="filler2.py" line="106"/>
+        <location filename="filler2.py" line="183"/>
         <source>&amp;Help</source>
         <translation>&amp;Pomoc</translation>
     </message>
     <message>
-        <location filename="filler2.py" line="107"/>
-        <source>toolBar</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="filler2.py" line="108"/>
+        <location filename="filler2.py" line="185"/>
         <source>&amp;Save data</source>
         <translation>&amp;Zapisz dane</translation>
     </message>
     <message>
-        <location filename="filler2.py" line="109"/>
-        <source>Save data to params.xml</source>
-        <translation>Zapisz dane do pliku params.xml</translation>
-    </message>
-    <message>
-        <location filename="filler2.py" line="110"/>
+        <location filename="filler2.py" line="187"/>
         <source>Ctrl+S</source>
         <translation>Ctrl+S</translation>
     </message>
     <message>
-        <location filename="filler2.py" line="111"/>
+        <location filename="filler2.py" line="188"/>
         <source>C&amp;lear</source>
         <translation>Wy&amp;czyść</translation>
     </message>
     <message>
-        <location filename="filler2.py" line="112"/>
+        <location filename="filler2.py" line="189"/>
         <source>Clear pending actions</source>
         <translation>Wyczyść zaplanowane akcje</translation>
     </message>
     <message>
-        <location filename="filler2.py" line="113"/>
+        <location filename="filler2.py" line="190"/>
         <source>Ctrl+C</source>
         <translation>Ctrl+C</translation>
     </message>
     <message>
-        <location filename="filler2.py" line="114"/>
+        <location filename="filler2.py" line="191"/>
         <source>E&amp;xit</source>
         <translation>Za&amp;kończ</translation>
     </message>
     <message>
-        <location filename="filler2.py" line="115"/>
+        <location filename="filler2.py" line="192"/>
         <source>Exit Filler 2</source>
         <translation>Zakończ program Filler 2</translation>
     </message>
     <message>
-        <location filename="filler2.py" line="116"/>
+        <location filename="filler2.py" line="193"/>
         <source>Ctrl+Q</source>
         <translation>Ctrl+Q</translation>
     </message>
     <message>
-        <location filename="filler2.py" line="117"/>
+        <location filename="filler2.py" line="194"/>
         <source>User&apos;s &amp;manual</source>
         <translation>Pod&amp;ręcznik użytkownika</translation>
     </message>
     <message>
-        <location filename="filler2.py" line="118"/>
+        <location filename="filler2.py" line="195"/>
         <source>Show user&apos;s manual</source>
         <translation>Pokaż podręcznik użytkownika</translation>
     </message>
     <message>
-        <location filename="filler2.py" line="120"/>
+        <location filename="filler2.py" line="197"/>
         <source>Con&amp;text help</source>
         <translation>Pomoc &amp;kontekstowa</translation>
     </message>
     <message>
-        <location filename="filler2.py" line="121"/>
+        <location filename="filler2.py" line="198"/>
         <source>Activate context help</source>
         <translation>Uruchom pomoc kontekstową</translation>
     </message>
     <message>
-        <location filename="filler2.py" line="123"/>
+        <location filename="filler2.py" line="200"/>
         <source>&amp;About</source>
         <translation>&amp;O programie</translation>
     </message>
     <message>
-        <location filename="filler2.py" line="124"/>
+        <location filename="filler2.py" line="201"/>
         <source>Information about Filler 2</source>
         <translation>Informacje o programie Filler 2</translation>
     </message>
     <message>
-        <location filename="filler2.py" line="125"/>
+        <location filename="filler2.py" line="202"/>
         <source>About &amp;Qt</source>
         <translation>O &amp;Qt</translation>
     </message>
     <message>
-        <location filename="filler2.py" line="126"/>
+        <location filename="filler2.py" line="203"/>
         <source>Information about Qt4</source>
         <translation>Informacje o Qt4</translation>
     </message>
     <message>
-        <location filename="filler2.py" line="119"/>
+        <location filename="filler2.py" line="196"/>
         <source>F1</source>
         <translation>F1</translation>
     </message>
     <message>
-        <location filename="filler2.py" line="122"/>
+        <location filename="filler2.py" line="199"/>
         <source>Shift+F1</source>
         <translation>Shift+F1</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="165"/>
+        <source>&quot;End&quot; date</source>
+        <translation>Początek okresu</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="166"/>
+        <source>Choose ending date</source>
+        <translation>Wybierz datę końca okresu</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="167"/>
+        <source>To:</source>
+        <translation>Do:</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="168"/>
+        <source>&quot;From&quot; date</source>
+        <translation>Koniec okresu</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="169"/>
+        <source>Choose beginning date</source>
+        <translation>Wybierz datę początku okresu</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="170"/>
+        <source>From:</source>
+        <translation>Od:</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="171"/>
+        <source>Parametr&apos;s value</source>
+        <translation>Wartość parametru</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="172"/>
+        <source>Enter parameter value</source>
+        <translation>Wpisz wartość parametru</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="173"/>
+        <source>List of sets</source>
+        <translation>Lista zestawów</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="174"/>
+        <source>Choose set from the list</source>
+        <translation>Wybierz zestaw z listy</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="175"/>
+        <source>Parameter list</source>
+        <translation>Lista parametrów</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="176"/>
+        <source>Choose parameter from the list</source>
+        <translation>Wybierz parametr z listy</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="177"/>
+        <source>Add change</source>
+        <translation>Dodaj zmianę</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="178"/>
+        <source>Add insertion to the table</source>
+        <translation>Dodaj zmianę do tabeli</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="179"/>
+        <source>Ctrl+=</source>
+        <translation>Ctrl+=</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="180"/>
+        <source>Table of changes</source>
+        <translation>Tabela zmian</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="181"/>
+        <source>Table of changes to be commited</source>
+        <translation>Tabela zmian do wprowadzenia w bazie</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="184"/>
+        <source>Toolbar</source>
+        <translation>Pasek narzędzi</translation>
+    </message>
+    <message>
+        <location filename="filler2.py" line="186"/>
+        <source>Save data to database</source>
+        <translation>Zapisz dane do bazy</translation>
     </message>
 </context>
 </TS>
