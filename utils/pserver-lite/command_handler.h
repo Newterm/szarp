af20a255bf29a24076ce9c1121bcadd10cf6a965
Tomasz Pieczerak <tph@newterm.pl>
2015-06-27 21:56:18 +0200

    pserver-lite: fix for compiler error on GCC < 4.8
    * GCC older then 4.8 does not support inheriting constructors (see:
      https://gcc.gnu.org/gcc-4.8/changes.html).

--- utils/pserver-lite/command_handler.h
+++ utils/pserver-lite/command_handler.h
@@ -5,6 +5,13 @@
 #include <vector>
 #include <stdexcept>
 
+// TODO: move this macro to some more appropriate place
+#ifndef GCC_VERSION
+#define GCC_VERSION (__GNUC__ * 10000 \
+                     + __GNUC_MINOR__ * 100 \
+                     + __GNUC_PATCHLEVEL__)
+#endif
+
 class CommandHandler {
 public:
 	static std::vector<std::string> tokenize (std::string& msg_received);
@@ -13,6 +20,7 @@ public:
 	virtual void exec (void) = 0;
 
 	/* Exception class definitions */
+#if GCC_VERSION >= 40800
 	class Exception : public std::runtime_error {
 		using std::runtime_error::runtime_error;
 	};
@@ -28,6 +36,33 @@ public:
 	class ConnectionError : public Exception {
 		using Exception::Exception;
 	};
+#else
+	class Exception : public std::runtime_error {
+	public:
+		explicit Exception (const std::string& what_arg)
+			: std::runtime_error(what_arg) { }
+	};
+	class ParseError : public Exception {
+	public:
+		explicit ParseError (const std::string& what_arg)
+			: Exception(what_arg) { }
+	};
+	class ArgumentError : public Exception {
+	public:
+		explicit ArgumentError (const std::string& what_arg)
+			: Exception(what_arg) { }
+	};
+	class SzbCacheError : public Exception {
+	public:
+		explicit SzbCacheError (const std::string& what_arg)
+			: Exception(what_arg) { }
+	};
+	class ConnectionError : public Exception {
+	public:
+		explicit ConnectionError (const std::string& what_arg)
+			: Exception(what_arg) { }
+	};
+#endif
 };
 
 #endif /* __COMMAND_HANDLER_H */
