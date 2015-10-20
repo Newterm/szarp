Newterm/szarp

commit 3f049c1deb868602b13fca68c70583f0cf80b1c0

draw3: MSW bug fix, don't mess with time wdg focus
it is again possible to switch period types using time
widget

wx/draw3/timewdg.cpp
@@ -44,7 +44,9 @@
 
 BEGIN_EVENT_TABLE(TimeWidget, wxScrolledWindow)
         EVT_RADIOBOX(wxID_ANY, TimeWidget::OnRadioSelected)
+#ifndef __WXMSW__
 	EVT_SET_FOCUS(TimeWidget::OnFocus)
+#endif
 END_EVENT_TABLE()
 
 class RadioButtonValidator : public wxValidator {
@@ -72,7 +74,9 @@ class RadioButtonValidator : public wxValidator {
 };
 
 BEGIN_EVENT_TABLE(RadioButtonValidator, wxValidator)
-	EVT_SET_FOCUS(RadioButtonValidator::OnFocus)
+#ifndef __WXMSW__
+	EVT_SET_FOCUS(TimeWidget::OnFocus)
+#endif
 END_EVENT_TABLE()
 
 

commit 7235c984371d7d99e895bc167f42eeb16f9129ea

draw3: fix regression on Linux introduced by 3f049c1d

wx/draw3/timewdg.cpp
@@ -75,7 +75,7 @@ class RadioButtonValidator : public wxValidator {
 
 BEGIN_EVENT_TABLE(RadioButtonValidator, wxValidator)
 #ifndef __WXMSW__
-	EVT_SET_FOCUS(TimeWidget::OnFocus)
+	EVT_SET_FOCUS(RadioButtonValidator::OnFocus)
 #endif
 END_EVENT_TABLE()
 