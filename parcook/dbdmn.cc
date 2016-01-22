Newterm/szarp

commit b34c4dc9d6bc0b52d7c4af1294be49c1ffb54050

* make the whole shebang compile...
... with g++ and boost that come with latest stable

parcook/dbdmn.cc
@@ -243,8 +243,7 @@ SZARP_PROBE_TYPE DbDaemon::GetProbeType(TParam * param, xmlNodePtr param_node)
 
 	std::string pt;
 	std::stringstream ss((char*)prop);
-	bool ok = ss >> pt;
-	if (!ok && !ss.eof()) {
+	if ((ss >> pt) && !ss.eof()) {
 		sz_log(1, "Invalid value %s for attribute extra:probe-type in line, %ld, using default PT_MIN10",
 		       	(char*)prop,  xmlGetLineNo(param_node));
 		xmlFree(prop);	

iks/server/utils/ptree.cpp
@@ -120,8 +120,8 @@ void ptree_to_xml( std::ostream& stream ,
                    bool pretty )
 {
 	auto settings = pretty ?
-		bp::xml_writer_make_settings(' ') :
-		bp::xml_writer_make_settings(' ', 4);
+		bp::xml_writer_make_settings<std::string>(' ') :
+		bp::xml_writer_make_settings<std::string>(' ', 4);
 
 	bp::xml_parser::write_xml( stream , ptree , settings );
 }

libSzarp2/include/sz4/definable_param_cache.h
@@ -83,7 +83,7 @@ public:
 	typedef boost::container::flat_map<time_type,
 			block_type*,
 			std::less<time_type>,
-			allocator_type<std::pair<const time_type, block_type*>
+			allocator_type<std::pair<time_type, block_type*>
 		> > map_type;
 #else
 	typedef std::map<time_type, block_type*> map_type;

libSzarp2/include/sz4/real_param_entry.h
@@ -68,7 +68,7 @@ public:
 template<class V, class T, class types> class real_param_entry_in_buffer {
 public:
 	typedef file_block_entry<V, T, types> file_block_entry_type;
-	typedef boost::container::flat_map<T, file_block_entry_type*, std::less<T>, allocator_type<std::pair<const T, file_block_entry_type*> > > map_type;
+	typedef boost::container::flat_map<T, file_block_entry_type*, std::less<T>, allocator_type<std::pair<T, file_block_entry_type*> > > map_type;
 	//typedef std::map<T, file_block_entry_type*, std::less<T>, allocator_type<std::pair<const T, file_block_entry_type*> > > map_type;
 private:
 	base_templ<types>* m_base;

libSzarp2/szarp_base_common/lua_parser_extra.cc
@@ -10,6 +10,7 @@
 #include <boost/spirit/include/phoenix_fusion.hpp>
 #include <boost/spirit/include/phoenix_stl.hpp>
 #include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/optional/optional_io.hpp>
 
 #include "szarp_base_common/lua_syntax.h"
 #include "lua_parser_extra.h"

unit_tests/sz4_block_unit_test.cpp
@@ -157,7 +157,7 @@ void Sz4BlockTestCase::pathParseTest() {
 }
 
 void Sz4BlockTestCase::blockLoadTest() {
-	char file_content[] = {
+	unsigned char file_content[] = {
 		 0x01, 0x00, 0x01, 0x00, 0x00, 0x00,
 		 0x02, 0x00, 0x02, 0x00, 0x00, 0x00,
 		 0x03, 0x00, 0x03, 0x00, 0x00, 0x00,
@@ -172,7 +172,7 @@ void Sz4BlockTestCase::blockLoadTest() {
 
 	{
 		std::ofstream ofs(SC::S2A(file_name.str()).c_str(), std::ios_base::binary);
-		ofs.write(file_content, sizeof(file_content));
+		ofs.write(reinterpret_cast<const char*>(file_content), sizeof(file_content));
 	}
 
 	std::vector<sz4::value_time_pair<short, unsigned> > v;

commit 7e68dde492aaed768911a87677c87a0a043b5790

fixed expression checking probe-type string
+ corrected error in first operand introduced in b34c4dc9d6bc0b52d7c4af1294be49c1ffb54050
+ corrected erroneous operation - should be or as we check if either the extraction from sstream failed or whether sth remained in it

parcook/dbdmn.cc
@@ -243,7 +243,7 @@ SZARP_PROBE_TYPE DbDaemon::GetProbeType(TParam * param, xmlNodePtr param_node)
 
 	std::string pt;
 	std::stringstream ss((char*)prop);
-	if ((ss >> pt) && !ss.eof()) {
+	if ((ss >> pt).fail() || !ss.eof()) {
 		sz_log(1, "Invalid value %s for attribute extra:probe-type in line, %ld, using default PT_MIN10",
 		       	(char*)prop,  xmlGetLineNo(param_node));
 		xmlFree(prop);	