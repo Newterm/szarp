af7725b827a13764d594058a6ecfb72a96baae36
mmorusiewicz <mmorusiewicz@matitude>
2016-09-08 15:54:49 +0200

    Feature: added sz4 capabilities to raporter.py

--- utils/raporter.py
+++ utils/raporter.py
@@ -21,11 +21,87 @@
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 """
 
-import curses, os, sys, traceback, unicodedata, threading, string, locale
+import curses, os, sys, traceback, unicodedata, threading, string, locale, time
 # To allow importing SZARP modules
 sys.path.append("/opt/szarp/lib/python")
 from libpar import LibparReader
 
+import zmq
+import paramsvalues_pb2
+
+class Param:
+	def __init__(self, param):
+		self.short_name = param.attrib['short_name'].encode(gb.fileencoding)
+		self.value = 0
+		self.unit = param.get('unit', "").encode(gb.fileencoding)
+		self.prec = int(param.get('prec', 0))
+
+	def getValue(self):
+		sv = str(self.value)
+		if self.prec == 0:
+			return (sv).encode(gb.fileencoding)
+		
+		while len(sv) < self.prec + 1:
+			sv = '0'+sv
+
+		return (sv[0:-self.prec]+"."+sv[-self.prec:]).encode(gb.fileencoding)
+
+	def getString(self):
+		return self.short_name+" = "+self.getValue()+" "+self.unit
+
+class Report:
+	def __init__(self):
+		self.params = []
+
+			
+class Params:
+	def __init__(self):
+		self.params = {} # name : Param
+		self.reports = {} # title : Report
+		self.indexes = {} # index : name
+		from lxml import etree
+		if gb.base is not None:
+			tree = etree.parse("/opt/szarp/"+gb.base+"/config/params.xml")
+		else:
+			tree = etree.parse("/etc/szarp/default/config/params.xml")
+
+		params2 = tree.xpath('//x:param',namespaces={'x': 'http://www.praterm.com.pl/SZARP/ipk'})
+		reports2 = tree.xpath('//x:raport',namespaces={'x': 'http://www.praterm.com.pl/SZARP/ipk'})
+		for r in reports2:
+			par_elem = r.getparent()
+			nm = par_elem.attrib['name'].encode(gb.fileencoding)
+			if nm not in self.params.keys():
+				self.params[nm] = Param(par_elem)
+				self.indexes[params2.index(par_elem)] = nm
+			rm = r.attrib['title'].encode(gb.fileencoding)
+			if rm not in self.reports.keys():
+				self.reports[rm] = Report()
+			self.reports[rm].params.append(nm)
+
+		self.initializeZmq()# might do this async-style
+
+	def initializeZmq(self):
+		if gb.hostname != "localhost":
+			self.hub_uri = "tcp://"+gb.hostname+":"+gb.portnumber
+		else:
+			self.hub_uri = LibparReader().get("parhub", "pub_conn_addr")
+
+		self.context = zmq.Context(1)
+		self.socket = self.context.socket(zmq.SUB)
+
+		self.socket.connect(self.hub_uri)
+		self.socket.setsockopt(zmq.SUBSCRIBE, "")
+		self.poller = zmq.Poller()
+		gb.sz4_ti = threading.Timer(1, update_zmq)
+		gb.sz4_ti.start()
+
+	def closeZmq(self):
+		try:
+			gb.sz4_ti.cancel();
+			del gb.sz4_ti
+		except:
+			pass
+	
 class gb:
 	"""
 	Global variables.
@@ -49,7 +125,35 @@ class gb:
 		fileencoding = "UTF-8"
 	fileencoding1 = "UTF-8"
 	ti = None # timer object
+	sz4_ti = None # timer object
 	timeinterval = 10 # screen refresh interval, if <=0 automatic screen refresh is off
+	sz4 = False
+	sz4_params = None
+	base = None
+
+
+def update_zmq():
+	gb.sz4_params.poller.register(gb.sz4_params.socket, zmq.POLLIN)
+	gb.sz4_params.poller.poll(10)
+	read_socket()
+	gb.sz4_ti = threading.Timer(gb.timeinterval, update_zmq)
+	gb.sz4_ti.start()
+	
+def read_socket():
+	msgs = {}
+	try:
+		msg = gb.sz4_params.socket.recv(zmq.NOBLOCK)
+		params_values = paramsvalues_pb2.ParamsValues.FromString(msg)
+		for param_value in params_values.param_values:
+			index = param_value.param_no
+			if index in gb.sz4_params.indexes.keys():
+				gb.sz4_params.params[gb.sz4_params.indexes[index]].value = param_value.int_value;
+			else:
+				pass
+	except zmq.ZMQError as e:
+		if e.errno != zmq.EAGAIN:
+			raise
+
 
 def getLocalParamdPort():
 	"""
@@ -100,11 +204,6 @@ def readReportsFromParamd(hostname):
 		#traceback.print_exc()
 		sys.exit(-1)
 	nodes = domtree.childNodes
-	del gb.params
-	del gb.reports
-	gb.reports = []
-	gb.params = []
-	gb.reports.append("Available reports: [/] - filter reports  [q or enter here] - quit")	
 	s = set()
 	for node in nodes[0].getElementsByTagName("list:raport"):
 		s.add(node.getAttribute("name").encode(gb.fileencoding))
@@ -112,6 +211,21 @@ def readReportsFromParamd(hostname):
 		gb.reports.append(element)
 	del domtree
 	del doc
+
+
+def readReports(hostname):
+	del gb.params
+	del gb.reports
+	gb.reports = []
+	gb.params = []
+	gb.reports.append("Available reports: [/] - filter reports  [q or enter here] - quit")	
+	
+	if gb.sz4 == True:
+		for r in gb.sz4_params.reports.keys():
+			gb.reports.append(r)
+	else:
+		readReportsFromParamd(hostname+":"+gb.portnumber)
+	
 	return gb.reports
 
 
@@ -126,7 +240,7 @@ def readParamsFromParamd(hostname, raportname):
 	except:
 		restoreScreen()
 		print "Cannot find host: " + hostname + " or report "+converseRaportName(raportname)+" is unavailable. Check if paramd is running."
-		#traceback.print_exc()
+		traceback.print_exc()
 		sys.exit(-1)
 	from xml.dom import minidom
 	try:
@@ -134,12 +248,9 @@ def readParamsFromParamd(hostname, raportname):
 	except:
 		restoreScreen()
 		print "Invalid xml data from paramd - cannot parse. Please update your paramd. (" , raportPath , ")"
-		#traceback.print_exc()
+		traceback.print_exc()
 		sys.exit(-1)		
 	nodes = domtree.childNodes
-	del gb.params
-	gb.params = []    
-	gb.params.append("Available parameters in "+raportname+" [enter here]-back [R/r]-refresh")
 	for node in nodes[0].getElementsByTagName("list:param"):
 		parametr = node.getAttribute("name")
 		l=parametr.rfind(":")
@@ -148,7 +259,19 @@ def readParamsFromParamd(hostname, raportname):
 		gb.params.append(text.encode(gb.fileencoding))
 	del domtree
 	del doc
+
+def readParams(hostname, raportname):
+	del gb.params
+	gb.params = []    
+	gb.params.append("Available parameters in "+raportname+" [enter here]-back [R/r]-refresh")
+
+	if gb.sz4 == True:
+		for n in gb.sz4_params.reports[raportname].params:
+			gb.params.append(n+" "+gb.sz4_params.params[n].getString())
+	else:
+		readParamsFromParamd(hostname+":"+gb.portnumber, gb.activereport)
 	return gb.params
+
 
 def autoRefreshInit():
 	"""
@@ -156,22 +279,22 @@ def autoRefreshInit():
 	"""	
 	if gb.timeinterval<=0: return
 	try:
-		gb.ti = threading.Timer(gb.timeinterval, autoRefreshDataFromParamd)
+		gb.ti = threading.Timer(gb.timeinterval, autoRefreshData)
 		gb.ti.start()
 	except:
 		restoreScreen()
 		traceback.print_exc()
 		sys.exit(-1)		
 	
-def autoRefreshDataFromParamd():
+def autoRefreshData():
 	"""
 	Performes automatic screen refresh 
 	"""	
 	if gb.timeinterval<=0: return
-	gb.cmdoutlines = readParamsFromParamd(gb.hostname+":"+gb.portnumber, gb.activereport)
+	gb.cmdoutlines = readParams(gb.hostname, gb.activereport)
 	updateScreen()
 	del gb.ti
-	gb.ti = threading.Timer(gb.timeinterval, autoRefreshDataFromParamd)
+	gb.ti = threading.Timer(gb.timeinterval, autoRefreshData)
 	gb.ti.start()
 
 def autoRefreshStop():
@@ -341,7 +464,7 @@ def main():
 			elif gb.whichmenu == 0 :
 				gb.whichmenu = 1 
 				gb.activereport = gb.cmdoutlines[gb.winrow+gb.startrow]
-				gb.cmdoutlines = readParamsFromParamd(gb.hostname+":"+gb.portnumber, gb.activereport)
+				gb.cmdoutlines = readParams(gb.hostname, gb.activereport)
 				gb.startrow = 1
 				gb.winrow = 0
 				goUpByOne()
@@ -356,10 +479,12 @@ def main():
 				goUpByOne()
 		elif c == "r" or c == "R": 
 			if gb.whichmenu == 1:	
-				gb.cmdoutlines = readParamsFromParamd(gb.hostname+":"+gb.portnumber, gb.activereport)
+				gb.cmdoutlines = readParams(gb.hostname, gb.activereport)
 				updateScreen()
 		elif c == "q": 
 			autoRefreshStop()
+			if gb.sz4 == True:
+				gb.sz4_params.closeZmq()
 			break
 		elif c == "/" and gb.whichmenu == 0:
 			filteringReports()
@@ -369,6 +494,7 @@ def restoreScreen():
 	"""
 	Restores screen to default
 	"""
+	if gb.sz4: gb.sz4_params.closeZmq()
 	if gb.scrn != None: 
 		curses.nocbreak()
 		curses.echo()
@@ -381,7 +507,15 @@ def getHostName(argv):
 	for i in range(1, len(argv)-1):
 		if argv[i]=="-h":
 			return argv[i+1]
-	return "localhost"		
+	return "localhost"
+
+def getBase(argv):
+	"""
+	"""
+	for i in range(1, len(argv)-1):
+		if argv[i]=="-b":
+			return argv[i+1]
+	return None
 
 def getPortNumber(argv):
 	"""
@@ -390,6 +524,9 @@ def getPortNumber(argv):
 	for i in range(1, len(argv)-1):
 		if argv[i]=="-p":
 			return argv[i+1]
+	if gb.sz4:
+		return "56662"
+	
 	return getLocalParamdPort()
 
 def getTimeInterval(argv):
@@ -404,28 +541,35 @@ def getTimeInterval(argv):
 				print "Invalid time interval"
 				sys.exit(-1)
 			return val
+	if gb.sz4:
+		return 1
 	return 10
 
 if __name__ == "__main__":
 	locale.setlocale(locale.LC_ALL, '')
-	if len(sys.argv) == 2 and sys.argv[1]=="--help":
-		print "usage: raporter.py [-p port] [-h hostname] [-t timeinterval] [--help]"
+	if "--help" in sys.argv:
+		print "usage: raporter.py [-p port] [-h host] [-t timeinterval] [-4 [-b base]] [--help]"
 		print "where:"
 		print "\t port - port number, if not available, it will be read from szarp.cfg"
-		print "\t hostname - hostname where paramd is running, default is localhost"
+		print "\t host - host where paramd or parhub is running, default is localhost"
 		print "\t timeinterval - screen refresh parameter, default is 10 seconds"
 		print "\t                if timeinterval<=0 then refreshing is off "
+		print "\t if -4 is specified, parhub is used instead of paramd"
+		print "\t base - will parse local params.xml from that prefix (sz4 only) and szarp default (/etc/szarp) otherwise"
+		print "\nexample usage:\n\traporter.py -h 10.99.0.6 -b kepn -4 - read /opt/szarp/kepn/config/params.xml and connect to parhub at 10.99.0.6"
+		print "\traporter.py -h 10.99.0.234 - connect to paramd at 10.99.0.234"
+		print "\traporter.py -4 - connect to local parhub and parse default config file"
 		sys.exit(1)
-	if len(sys.argv) % 2 == 0:
-		print "usage: raporter.py [-p port] [-h hostname] [-t timeinterval] [--help]"
-		sys.exit(1)
-
+	
+	gb.sz4 = '-4' in sys.argv
 	gb.hostname = getHostName(sys.argv)
 	gb.portnumber = getPortNumber(sys.argv)
+	if gb.sz4 == True:
+		gb.base = getBase(sys.argv)
+		gb.sz4_params = Params()
 	gb.timeinterval = getTimeInterval(sys.argv)
-	gb.cmdoutlines = readReportsFromParamd(gb.hostname+":"+gb.portnumber)
+	gb.cmdoutlines = readReports(gb.hostname)
 	try:
 		main()
 	except:
 		restoreScreen()
-		#traceback.print_exc()
