063b1a42ff1ee95ceab195425c4eae4911863f19
Mateusz Morusiewicz <mmorusiewicz@newterm.pl>
2016-11-26 16:18:27 +0100

    Draw3: better error handling on import

--- libSzarp2/include/xmlutils.h
+++ libSzarp2/include/xmlutils.h
@@ -24,6 +24,8 @@
 #include <libxml/xpath.h>
 #include <libxml/xpathInternals.h>
 #include <string>
+#include <memory>
+#include "exception.h"
 
 /** Returns pointer to XML node described by XPath expresion.
  * @param xpath_expr expresion describing node (exactly one!)
@@ -47,6 +49,37 @@ xmlChar *uxmlXPathGetProp(const xmlChar *xpath_expr,
  * (may contain non-printable chars as TAB, FORM FEED)
  */
 std::string xmlToString(xmlDocPtr doc);
+
+class XmlDocException: public SzException {
+	SZ_INHERIT_CONSTR(XmlDocException, SzException)
+};
+
+class szXmlDoc {
+public:
+	szXmlDoc(xmlDoc* root): root_(root) { 
+		if (root == NULL) { throw XmlDocException("Failed to load xml."); }
+	}
+
+	szXmlDoc(const char* path): root_(xmlParseFile(path)) {
+		if (root_ == NULL) { throw XmlDocException(std::string("Failed to read file")+std::string(path)); }
+	}
+
+	~szXmlDoc() { xmlFreeDoc(root_); }
+
+	xmlDoc& operator->() { return *root_; }
+	xmlDoc& operator*() { return *root_; }
+
+	static std::unique_ptr<szXmlDoc> getDoc(xmlDoc* root) {
+		return std::unique_ptr<szXmlDoc>(new szXmlDoc(root));
+	}
+
+	static std::unique_ptr<szXmlDoc> getDoc(const char* root) {
+		return std::unique_ptr<szXmlDoc>(new szXmlDoc(root));
+	}
+
+private:
+	xmlDoc *root_;
+};
 
 #endif /* LIBXML_XPATH_ENABLED */
 
