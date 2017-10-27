baca3a5dbb7b0b1cd2abc1e2a414cadecff46a26
Michał Glinka <mglinka@newterm.pl>
2017-10-27 12:53:35 +0200

    Delete wx and extern

--- extern/ttyproxy/ttyproxy.h
+++
@@ -1,8 +0,0 @@
-#define TERMIOS_CHANGE 	0
-#define SET_DTR 	1
-#define CLEAR_DTR 	(1 << 1)
-#define SET_RTS		(1 << 2) 
-#define CLEAR_RTS	(1 << 4) 
-
-#define TTYPROXY_MAGIC 'T'
-#define TTYPROXY_IOCSREADY _IO(TTYPROXY_MAGIC, 2)
