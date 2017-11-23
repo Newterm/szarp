6b74bc23f5d7eeb2b4415b83a0c5901a7d03bc53
Aleksy Barcz <abarcz@newterm.pl>
2017-11-23 07:50:08 +0100

    moved pyipk to separate repo

--- pyipk/libipk/__init__.py
+++
@@ -1,1 +0,0 @@
-__all__ = [ "params" , "plugins" , "plugin" , "errors" ]
--- pyipk/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo '-------------------------- Running python 2 --------------------------'
-env PYTHONPATH="${PYTHONPATH}:." python -m unittest discover
-echo '-------------------------- Running python 3 --------------------------'
-#env PYTHONPATH="${PYTHONPATH}:." python3 -m unittest discover
