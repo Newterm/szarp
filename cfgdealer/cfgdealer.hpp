e7944164fda022c44223d02646dca3db16875517
Mateusz Morusiewicz <mmorusiewicz@newterm.pl>
2017-12-05 15:34:32 +0100

    Cfgdealer: modified processing sends, added processing sends from <defined>

--- cfgdealer/cfgdealer.hpp
+++ cfgdealer/cfgdealer.hpp
@@ -12,24 +12,34 @@ public:
 	CfgDealer(const ArgsManager& args_mgr);
 	void serve();
 
-	const boost::property_tree::wptree get_device_config(const size_t device_no);
+	using wptree = boost::property_tree::wptree;
+	const wptree get_device_config(const size_t device_no);
 
 private:
 	void process_request(zmq::message_t& request);
+	void send_reply(const std::string& str);
 
-	void output_device_config(const boost::property_tree::wptree& device_config);
+	void output_device_config(const wptree& device_config);
 
 	void configure_socket(const ArgsManager&);
-	void prepare_configs(const ArgsManager&);
+
+	void prepare_config(const ArgsManager&);
+
+	void prepare_units();
+	void prepare_params();
+	void prepare_send_from_param(const std::pair<std::wstring, wptree>& send_pair, size_t& param_ind);
 
 private:
-	const size_t NO_PROCESSING_THREADS = 1;
+	const int NO_PROCESSING_THREADS = 1;
 	zmq::context_t context{NO_PROCESSING_THREADS};
 	std::unique_ptr<zmq::socket_t> socket;
-	boost::property_tree::wptree tree;
+	wptree tree;
 
-	std::unordered_map<std::wstring, std::pair<boost::property_tree::wptree, size_t>> sends;
+	std::unordered_set<std::wstring> sends_to_add;
+
+	std::unordered_map<std::wstring, std::pair<wptree, size_t>> sends;
 	size_t no_devices;
+
 };
 
 #endif
