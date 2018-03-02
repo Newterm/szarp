50d8d53339bd9d2380df5858997edfe897c4a9c6
Grzegorz Wojciechowski <gwojciech@newterm.pl>
2018-03-02 10:55:36 +0100

    draw3: graphs thickness fix
    * setting initial graphs thickness in draw3 config

--- wx/draw3/drawpnl.cpp
+++ wx/draw3/drawpnl.cpp
@@ -1081,27 +1081,34 @@ void DrawPanel::SetActive(bool _active) {
 
 		menu_bar->FindItem(XRCID("SplitCursor"))->Check(dc->GetDoubleCursor());
 
-		int thickness = wxConfig::Get()->Read(_T("GRAPHS_THICKNESS"), thickness);
-		switch(thickness) {
-			case 1:
-				menu_bar->FindItem(XRCID("Thickness1"))->Check(true);
-				break;
-			case 2:
-				menu_bar->FindItem(XRCID("Thickness2"))->Check(true);
-				break;
-			case 3:
-				menu_bar->FindItem(XRCID("Thickness3"))->Check(true);
-				break;
-			case 4:
-				menu_bar->FindItem(XRCID("Thickness4"))->Check(true);
-				break;
-			case 5:
-				menu_bar->FindItem(XRCID("Thickness5"))->Check(true);
-				break;
-			default:
-				thickness = 2;
-				menu_bar->FindItem(XRCID("Thickness2"))->Check(true);
-				ASSERT(!"Bad thickness!");
+		int thickness;
+		if ( thickness = wxConfig::Get()->ReadLong(_T("GRAPHS_THICKNESS"), thickness)) {
+			switch(thickness) {
+				case 1:
+					menu_bar->FindItem(XRCID("Thickness1"))->Check(true);
+					break;
+				case 2:
+					menu_bar->FindItem(XRCID("Thickness2"))->Check(true);
+					break;
+				case 3:
+					menu_bar->FindItem(XRCID("Thickness3"))->Check(true);
+					break;
+				case 4:
+					menu_bar->FindItem(XRCID("Thickness4"))->Check(true);
+					break;
+				case 5:
+					menu_bar->FindItem(XRCID("Thickness5"))->Check(true);
+					break;
+				default:
+					thickness = 2;
+					menu_bar->FindItem(XRCID("Thickness2"))->Check(true);
+					ASSERT(!"Bad thickness!");
+			}
+		} else {
+			thickness = 2;
+			menu_bar->FindItem(XRCID("Thickness2"))->Check(true);
+			wxConfig::Get()->Write(_T("GRAPHS_THICKNESS"), thickness);
+			wxConfig::Get()->Flush();
 		}
 
 		dg->SetGraphThickness(thickness);
