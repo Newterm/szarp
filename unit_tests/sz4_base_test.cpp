453f4e86415aa47468913ed9d28a65a45bfc6ef8
Mateusz Morusiewicz <mmorusiewicz@newterm.pl>
2018-07-13 15:07:58 +0200

    Feature branch changes fixup

--- unit_tests/sz4_base_test.cpp
+++ unit_tests/sz4_base_test.cpp
@@ -147,7 +147,7 @@ R"(<?xml version="1.0" encoding="utf-8"?>
 		}
 	}
 
-	ParamCachingIPKContainer::Init(m_base_dir_name, L"/opt/szarp", L"pl");
+	IPKContainer::Init(m_base_dir_name, L"/opt/szarp", L"pl");
 
 }
 
