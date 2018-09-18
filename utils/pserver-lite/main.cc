f5a948bed42570e17db0230c3a790c5bbbc442d3
Paweł Ciechomski <pciechomski@newterm.pl>
2018-09-18 15:32:49 +0200

    Update license to GPLv3

--- utils/pserver-lite/main.cc
+++ utils/pserver-lite/main.cc
@@ -88,7 +88,7 @@ int main (int argc, char **argv)
 				<< PROGRAM_NAME << " " << VERSION_STR
 				<< ", SZARP Probes Server LITE.\n\n"
 				<< "Copyright (C) 2015 Newterm\n"
-				<< "License GPLv2+: GNU GPL version 2 or later\n"
+				<< "License GPLv3: GNU GPL version 3 or later\n"
 				<< "<http://www.gnu.org/licenses/gpl.html>.\n"
 				<< "This is free software: you are free to change and redistribute it.\n"
 				<< "There is NO WARRANTY, to the extent permitted by law.\n\n"
